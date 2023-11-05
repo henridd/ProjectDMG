1b41fda08bbfa961599212bfcf23d460be92caf2
henridd <henri.oo@hotmail.com>
2023-11-05 17:43:41 +0100

    Refactoring + Testing some addresses

--- Plugins/ProjectDMG.PokemonRedElasticsearchIntegration/Bootstrapper.cs
+++ Plugins/ProjectDMG.PokemonRedElasticsearchIntegration/Bootstrapper.cs
@@ -1,21 +1,42 @@
 ﻿using ProjectDMG.Api;
 using ProjectDMG.Api.Notifications;
+using System;
 using System.Diagnostics;
 
 namespace ProjectDMG.PokemonRedElasticsearchIntegration
 {
     public class Bootstrapper : ProjectDMGPlugin
     {
-        private const string Pokemon1HP = "D16D";
-
         public override void Run()
         {
-            MemoryWatcherProvider.GetInstance().AddSubscription(Pokemon1HP, null).ItemAdded += OnMoneyUpdated;
+            var memoryWatcher = MemoryWatcherProvider.GetInstance();
+
+            memoryWatcher.AddSubscription(MemoryAddresses.TurnNumber, null).ItemAdded += TurnNumberChanged;
+            memoryWatcher.AddSubscription(MemoryAddresses.Pokemon1HP, null).ItemAdded += OnPlayerHPChanged;
+            memoryWatcher.AddSubscription(MemoryAddresses.EnemyHP, null).ItemAdded += OnEnemyHPChanged;
+
         }
 
-        private void OnMoneyUpdated(object? sender, ItemAddedEventArgs<MemoryAddressUpdatedNotification> e)
+        private void TurnNumberChanged(object? sender, ItemAddedEventArgs<MemoryAddressUpdatedNotification> e)
         {
-            Debug.WriteLine($"Pokemon1HP changed. {e.Item.AddressesValues[Pokemon1HP].PreviousValue} --> {e.Item.AddressesValues[Pokemon1HP].NewValue}");
+            var turnAddressUpdate = e.Item.AddressesValues[MemoryAddresses.TurnNumber];
+            if(turnAddressUpdate.NewValue == 0)
+            {
+                Debug.WriteLine("Battle started!");
+                return;
+            }
+
+            Debug.WriteLine($"Starting turn {turnAddressUpdate.NewValue}");
+        }
+
+        private void OnEnemyHPChanged(object? sender, ItemAddedEventArgs<MemoryAddressUpdatedNotification> e)
+        {
+            Debug.WriteLine($"EnemyHP changed. {e.Item.AddressesValues[MemoryAddresses.EnemyHP].PreviousValue} --> {e.Item.AddressesValues[MemoryAddresses.EnemyHP].NewValue}");
+        }
+
+        private void OnPlayerHPChanged(object? sender, ItemAddedEventArgs<MemoryAddressUpdatedNotification> e)
+        {
+            Debug.WriteLine($"Pokemon1HP changed. {e.Item.AddressesValues[MemoryAddresses.Pokemon1HP].PreviousValue} --> {e.Item.AddressesValues[MemoryAddresses.Pokemon1HP].NewValue}");
         }
     }
 }
---
+++ Plugins/ProjectDMG.PokemonRedElasticsearchIntegration/MemoryAddresses.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectDMG.PokemonRedElasticsearchIntegration
+{
+    internal static class MemoryAddresses
+    {
+        public const string Pokemon1HP = "D16D";
+
+        // Battle
+        public const string TurnNumber = "CCD5";
+        public const string EnemyHP = "CFE7";
+        public const string PlayerBattleMove = "CCDC";
+    }
+}
