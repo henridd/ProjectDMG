bf072226c8b73cf1a34d6a090632613e7a36db79
henridd <henri.oo@hotmail.com>
2023-11-09 21:20:00 +0100

    More tests

--- Plugins/ProjectDMG.PokemonRedElasticsearchIntegration/Bootstrapper.cs
+++ Plugins/ProjectDMG.PokemonRedElasticsearchIntegration/Bootstrapper.cs
@@ -2,6 +2,7 @@
 using ProjectDMG.Api.Notifications;
 using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace ProjectDMG.PokemonRedElasticsearchIntegration
 {
@@ -11,16 +12,49 @@ namespace ProjectDMG.PokemonRedElasticsearchIntegration
         {
             var memoryWatcher = MemoryWatcherProvider.GetInstance();
 
+            var enemyNameRange = new AddressRange(new []
+            {
+                MemoryAddresses.EnemyName1,
+                MemoryAddresses.EnemyName2,
+                MemoryAddresses.EnemyName3,
+                MemoryAddresses.EnemyName4,
+                MemoryAddresses.EnemyName5,
+                MemoryAddresses.EnemyName6,
+                MemoryAddresses.EnemyName7,
+                MemoryAddresses.EnemyName8,
+                MemoryAddresses.EnemyName9,
+                MemoryAddresses.EnemyName10,
+            });
+            memoryWatcher.AddSubscription(enemyNameRange, null).ItemAdded += EnemyNameChanged;
+
+            var moneyRange = new AddressRange(new[]
+            {
+                MemoryAddresses.Money1,
+                MemoryAddresses.Money2,
+                MemoryAddresses.Money3
+            });
+            memoryWatcher.AddSubscription(moneyRange, null).ItemAdded += MoneyChanged;
+
             memoryWatcher.AddSubscription(MemoryAddresses.TurnNumber, null).ItemAdded += TurnNumberChanged;
             memoryWatcher.AddSubscription(MemoryAddresses.Pokemon1HP, null).ItemAdded += OnPlayerHPChanged;
             memoryWatcher.AddSubscription(MemoryAddresses.EnemyHP, null).ItemAdded += OnEnemyHPChanged;
 
         }
 
+        private void MoneyChanged(object? sender, ItemAddedEventArgs<MemoryAddressUpdatedNotification> e)
+        {
+            Debug.WriteLine("Money name changed");
+        }
+
+        private void EnemyNameChanged(object? sender, ItemAddedEventArgs<MemoryAddressUpdatedNotification> e)
+        {
+            Debug.WriteLine("Enemy name changed");
+        }
+
         private void TurnNumberChanged(object? sender, ItemAddedEventArgs<MemoryAddressUpdatedNotification> e)
         {
             var turnAddressUpdate = e.Item.AddressesValues[MemoryAddresses.TurnNumber];
-            if(turnAddressUpdate.NewValue == 0)
+            if(turnAddressUpdate.NewValue.First() == 0)
             {
                 Debug.WriteLine("Battle started!");
                 return;
--- Plugins/ProjectDMG.PokemonRedElasticsearchIntegration/MemoryAddresses.cs
+++ Plugins/ProjectDMG.PokemonRedElasticsearchIntegration/MemoryAddresses.cs
@@ -2,9 +2,24 @@
 {
     internal static class MemoryAddresses
     {
+        public const string Money1 = "D347";
+        public const string Money2 = "D348";
+        public const string Money3 = "D349";
+
         public const string Pokemon1HP = "D16D";
 
         // Battle
+        public const string EnemyName1 = "CFDA";
+        public const string EnemyName2 = "CFDB";
+        public const string EnemyName3 = "CFDC";
+        public const string EnemyName4 = "CFDD";
+        public const string EnemyName5 = "CFDE";
+        public const string EnemyName6 = "CFDF";
+        public const string EnemyName7 = "CFE0";
+        public const string EnemyName8 = "CFE1";
+        public const string EnemyName9 = "CFE2";
+        public const string EnemyName10 = "CFE3";
+
         public const string TurnNumber = "CCD5";
         public const string EnemyHP = "CFE7";
         public const string PlayerBattleMove = "CCDC";
